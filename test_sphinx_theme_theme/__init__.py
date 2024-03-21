3bcff5d0790aacf63f0e04cd25c6d81fc520cdab
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-03-21 13:48:25 -0400

    Update test_sphinx_theme

--- test_sphinx_theme_theme/__init__.py
+++ test_sphinx_theme_theme/__init__.py
@@ -1,4 +1,12 @@
 """
 Seriously?
 """
+import os
+
 __version__ = "0.0.1"
+
+def setup(app):
+    theme_path = os.path.abspath(os.path.dirname(__file__))
+    app.add_html_theme("test_sphinx_theme_theme", theme_path)
+    # app.connect("html-page-context", update_context)
+    # return {"version": __version__, "parallel_read_safe": True}
